015be9a4fbee2a3127323ea0807f0f9a855d68ea
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-02-03 16:21:44 +0000

    c++

--- m1t1-hello world.cpp
+++ m1t1-hello world.cpp
@@ -10,5 +10,12 @@ jamesk
 using namespace std; 
 
 int main() {
+    //Greet the user
+    cout << "Hello,user!"<< endl; //this is endline 
+    cout <<"What's your name?"<<endl;
+    string name; 
+    cin>> name;
+    cout<<"Nice to meet you,"<<name<<endl
+
     return 0;
 }
