524abab1e188bf742bb024924f34ec35252a9195
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-03-03 22:24:12 +0000

    started m3lab

---
+++ M3LAB.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M3LAB
+//Keora James 
+// 3/3/226
+
+#include <iostream>
+using namespace std;
+
+// This program will ask a ask question and then respond
+// Declare the variable we need 
+int choice; 
+
+// ask the question 
+cout << "Hello, player! Choose between the 3 doors"<< endl; 
+cout<< "But be careful! The Chainsaw Man is lurking between 2 doors"<< 
+cout<< "Type 1-3: "
+cin >> choice;
