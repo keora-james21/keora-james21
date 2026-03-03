61c2efe39c699ee6dc1caac29c95fa9d64ab9b68
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-03-03 22:51:40 +0000

    finished m2lab2

---
+++ M3LAB2.cpp
@@ -0,0 +1,45 @@
+// CSC 134 
+// M3LAB2
+// Keora James 
+// 3/3/2026
+
+
+#include <iostream>
+using namespace std; 
+
+int main() {
+    double grade;
+
+    // Greet student 
+    cout<< " Hello, student! Please enter your first and last name to access your grade for C++"<< endl;
+    cin>> first_name
+    cout << "Last name?"<< endl; 
+    cin>> last_name 
+    full_name= first_name + last_name 
+    cout << "Grettings "<< full_name<< "! Here is the summary for your grades!"<< endl;
+
+    // Enter numeric grades 
+    cout<< "Enter your numberic grade (0-100): "<< endl; 
+    cin>> grade 
+    if (grade < 0 || grade>100){
+        cout << "Invalid. Please enter a number between 0 and 100"<<endl;
+    
+    }
+    else if (grade >=90){
+        cout<< "Your grade is an A"<< endl;
+    }
+    else if (grade>=80){
+        cout<< "Your grade is a B"<< endl;
+        }
+        else if (grade>=70){
+            cout << "Your grade is a C"<< endl;
+        }
+        else if (grade>=60){
+            cout<< "Your grade is a D"<< endl;
+        
+        }
+        else if (grade<=50){
+            cout << "Your grade is a F"<< endl;
+        }
+        return 0;
+}
