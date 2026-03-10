18e72d453033694e1ff23191071d407561e3e877
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-03-10 13:22:01 +0000

    finished m4t1

--- M4T1.cpp
+++ M4T1.cpp
@@ -14,7 +14,22 @@ int main()
     int count = 1;
     while (count < 5) {
         cout << "Hello number " << count << endl;
+        // count = count + 1;
+        count++; // shorter version of the above line
     }
+cout << "That's all" << endl;
+// program 5-6
+const int MIN_NUMBER = 1;
+const int MAX_NUMBER = 10;
 
+int num = MIN_NUMBER;
+// print table number 
+cout << "Number  Number Squared" << endl;
+
+
+while (num <= MAX_NUMBER) {
+    cout << num << " "<< num*num << endl;
+    num++;
+}
     return 0; // done
 }
