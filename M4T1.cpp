a9bdb4769bf1d3b01c38e09d2536a67fcf0f3a6b
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-03-10 13:04:27 +0000

    started m1t1

---
+++ M4T1.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M4T1 - While
+// Keora James 
+// 3/10/2026
+
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+
+    // Part 1, just say "hi" five times
+    int count = 1;
+    while (count < 5) {
+        cout << "Hello number " << count << endl;
+    }
+
+    return 0; // done
+}
