f42a9278ebe9060dbe54bc95eba001adefc6e902
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-03-24 23:18:41 +0000

    started m4hw1 started

---
+++ M4HW1.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M4HW1- Silver
+// Keora James 
+// 3/24/26
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double num1, num2; 
+    cout <<" Enter the first number of the probelm"<<end;
+    cin>> num1;
+
+    cout<< "Enter the second number of the problem"<<endl;
+    cin>> num2; 
+
+    double result= num1*num2;
+    cout << num1<< "times"<< num2 << "is "<<result<< endl;
+}
