7b66c53b576c8bbbc591cc48d7bfe73a92a9f634
keora-james21 <146122168+keora-james21@users.noreply.github.com>
2026-03-24 23:53:29 +0000

    finished m4hw1

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -8,13 +8,40 @@
 using namespace std;
 
 int main() {
+    // Question 1
+    cout << "Question 1" << endl;
     double num1, num2; 
-    cout <<" Enter the first number of the probelm"<<end;
+    cout <<"Enter the first number of the probelm"<<endl;
     cin>> num1;
 
     cout<< "Enter the second number of the problem"<<endl;
     cin>> num2; 
 
     double result= num1*num2;
-    cout << num1<< "times"<< num2 << "is "<<result<< endl;
+    cout << num1<< " times "<< num2 << " is "<<result << endl;
+
+cout << "____________________________________________________________" << endl;
+
+    //Question 2
+    cout << "Question 2" << endl;
+    const int MIN_NUMBER = 1;
+    const int MAX_NUMBER = 12;
+    double num3, num4;
+
+    cout << "Enter the first number 1-12" << endl;
+    cin >> num3;
+if (num3 < MIN_NUMBER || num3 > MAX_NUMBER) {
+        cout << "Invalid input. Please enter a number between " << MIN_NUMBER << " and " << MAX_NUMBER << "." << endl;
+    } else {
+    cout << "Enter the second number 1-12" << endl;
+    cin >> num4;
+if (num4 < MIN_NUMBER || num4 > MAX_NUMBER) {
+        cout << "Invalid input. Please enter a number between " << MIN_NUMBER << " and " << MAX_NUMBER << "." << endl;
+    } else {
+        double result2 = num3 * num4;
+        cout << num3 << " times " << num4 << " is " << result2 << endl;
+    }
+    }
+
+    return 0;
 }
